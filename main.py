dccd9f4d8cf41838c48d332d0769c89ab85c7aaa
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-09 01:21:01 -0400

    added test_subtraction test cases

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.operations import operations
-
-print(operations.additon(12,2))
-print(operations.additon(12,None))
-print(operations.additon(12,2))
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,6 +4,10 @@ from typing import Union
 
 Number = Union[int,float]
 
+#---------------------------------------
+# Unit tests for addition method
+#---------------------------------------
+
 @pytest.mark.parametrize("a,b,expected",
                          [(1,2,3), # adding two positive ints
                           (0,0,0), # adding two zeros
@@ -24,4 +28,28 @@ def test_addition(a: Number, b: Number, expected: Number) -> None:
 
     result = operations.additon(a,b)
     assert result == expected, f"Expected addition({a},{b}) to be {expected} but got {result}"
-    
+
+#---------------------------------------
+# Unit tests for subtraction method
+#---------------------------------------
+
+@pytest.mark.parametrize("a, b, expected",
+                         [(2,1,1), # subtracting two positive ints
+                          (0,0,0), # subtracting two zeros
+                          (1,-1,2), # subtracting a a negrative from a positive int
+                          (-1,-1,0) # subtracting a negative int from negative int
+                          (2.5,1.5,1.0) # subtracting two positive floats
+                          (-1.5,2.5,-4.0)], #subtracting negative and positive float
+                          ids=[
+                              "subtract_two_positive_ints",
+                              "subtract_ two_zeros",
+                              "subtracting_negative_and_positive_int",
+                              "subtract_two_negative_ints",
+                              "subtract_two_positive_floats",
+                              "subtract_negative_and_positive_floats"
+                          ]
+                          )
+def test_subtraction(a: Number, b: Number, expected: Number) -> None:
+    result = operations.subtraction(a,b)
+    assert result == expected, f"expected subtraction({a},{b} to be {expected} but got {result})"
+
